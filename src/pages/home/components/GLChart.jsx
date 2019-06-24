fdda6b8c87c8d9b3636c8690d551793c404e5c2c
ETBox <13318972468@126.com>
2019-06-24 15:42:56 +0800

    feat: add 3D gaussian

--- src/pages/home/components/GLChart.jsx
+++ src/pages/home/components/GLChart.jsx
@@ -10,95 +10,58 @@ export default class GLChart extends Component {
 
 	EventsDict = {}
 
+	getData = () => {
+		function makeGaussian(amplitude, x0, y0, sigmaX, sigmaY) {
+			return function(amplitude, x0, y0, sigmaX, sigmaY, x, y) {
+				var exponent = -(
+					Math.pow(x - x0, 2) / (2 * Math.pow(sigmaX, 2)) +
+					Math.pow(y - y0, 2) / (2 * Math.pow(sigmaY, 2))
+				)
+				return amplitude * Math.pow(Math.E, exponent)
+			}.bind(null, amplitude, x0, y0, sigmaX, sigmaY)
+		}
+		// 创建一个高斯分布函数
+		var gaussian = makeGaussian(50, 0, 0, 20, 20)
+
+		var data = []
+		for (var i = 0; i < 1000; i++) {
+			// x, y 随机分布
+			var x = Math.random() * 100 - 50
+			var y = Math.random() * 100 - 50
+			var z = gaussian(x, y)
+			data.push([x, y, z])
+		}
+		return data
+	}
+
 	getOption = () => ({
 		grid3D: {},
 		xAxis3D: {},
 		yAxis3D: {},
 		zAxis3D: {},
 		series: [
+			// {
+			// 	type: 'scatter3D',
+			// 	symbolSize: 50,
+			// 	data: [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
+			// 	itemStyle: {
+			// 		opacity: 1,
+			// 	},
+			// },
 			{
 				type: 'scatter3D',
-				symbolSize: 50,
-				data: [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
-				itemStyle: {
-					opacity: 1,
-				},
+				data: this.getData(),
 			},
 		],
 	})
 
 	render() {
-		// let data = []
-		// // Parametric curve
-		// for (let t = 0; t < 25; t += 0.001) {
-		// 	const x = (1 + 0.25 * Math.cos(75 * t)) * Math.cos(t)
-		// 	const y = (1 + 0.25 * Math.cos(75 * t)) * Math.sin(t)
-		// 	const z = t + 2.0 * Math.sin(75 * t)
-		// 	data.push([x, y, z])
-		// }
-		// console.log(data.length)
-
-		// const option = {
-		// 	tooltip: {},
-		// 	backgroundColor: '#fff',
-		// 	visualMap: {
-		// 		show: false,
-		// 		dimension: 2,
-		// 		min: 0,
-		// 		max: 30,
-		// 		inRange: {
-		// 			color: [
-		// 				'#313695',
-		// 				'#4575b4',
-		// 				'#74add1',
-		// 				'#abd9e9',
-		// 				'#e0f3f8',
-		// 				'#ffffbf',
-		// 				'#fee090',
-		// 				'#fdae61',
-		// 				'#f46d43',
-		// 				'#d73027',
-		// 				'#a50026',
-		// 			],
-		// 		},
-		// 	},
-		// 	xAxis3D: {
-		// 		type: 'value',
-		// 	},
-		// 	yAxis3D: {
-		// 		type: 'value',
-		// 	},
-		// 	zAxis3D: {
-		// 		type: 'value',
-		// 	},
-		// 	grid3D: {
-		// 		viewControl: {
-		// 			projection: 'orthographic',
-		// 		},
-		// 	},
-		// 	series: [
-		// 		{
-		// 			type: 'line3D',
-		// 			data: data,
-		// 			lineStyle: {
-		// 				width: 4,
-		// 			},
-		// 		},
-		// 	],
-		// }
-
 		return (
 			<div>
-				{/* <ReactEcharts
-					option={option}
-					notMerge={true}
-					lazyUpdate={true}
-					theme={'theme_name'}
-					// onChartReady={this.onChartReadyCallback}
-					onEvents={this.EventsDict}
-					opts={{ renderer: 'svg' }}
-				/> */}
-				<ReactEcharts option={this.getOption()} />
+				<ReactEcharts
+					option={this.getOption()}
+					style={{ width: '100%', height: '500px' }}
+				/>
 			</div>
 		)
 	}
